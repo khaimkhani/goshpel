a3775028c575bb9106d98e4c890a6b2563513028
tkay <taimor.khan@mail.utoronto.ca>
2024-08-27 12:45:25 -0400

    diff approach

--- listen.go
+++ listen.go
@@ -1,42 +1,53 @@
-/*
-Largely experimental. I'm not even sure if this is possible.
-The elementary idea currently is having a never ending goroutine being fed strings through a channel which get executed.
-
-In reality this probably needs something a bit more involved.
-
-*/
-
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"os"
-	"path/filepath"
+	"strings"
 )
 
-type ExecChannel chan string
+var SHELLPATH string = "/var/lib/goshpel/shell.go"
 
 func main() {
+	ReadStdin()
+}
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println(filepath.Glob("*"))
-	fmt.Print(">> ")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+func ReadStdin() {
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Print(">> ")
+		eof := scanner.Scan()
+		text := scanner.Text()
+		fmt.Println(text)
+		fmt.Println(eof)
+	}
+}
+
+func SyntaxCheck(text string) (bool, error) {
+
+	para := strings.Count(text, "(") == strings.Count(text, ")")
+	curly := strings.Count(text, "{") == strings.Count(text, "}")
+
+	// invalid
+	square := strings.Count(text, "[") == strings.Count(text, "]")
+	if square {
+		return false, bufio.ErrBadReadCount
+	}
+
+	return !(para && curly), nil
 
 }
 
-// this is a subroutine
-func listener(ec ExecChannel) {
-	for {
-		// exec here
-		var x chan string
-		select {
+func AppendToFile(text string) error {
 
-		case x <- ec:
-			// exec(x)
-
-		}
+	fi, err := os.Open(SHELLPATH)
+	if err != nil {
+		return err
 	}
+
+	defer fi.Close()
+
+	// is valid expression + shell returned error
+	return bufio.ErrFinalToken
 }
